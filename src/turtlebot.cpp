cfb8a7d9c27adadfa92c621cbf1c98e126695484
MarkzcZ <113441407+MarkzcZ@users.noreply.github.com>
2022-11-10 14:56:21 +0800

    Add files via upload

---
+++ src/turtlebot.cpp
@@ -0,0 +1,106 @@
+/** MIT License
+Copyright (c) 2017 Sudarshan Raghunathan
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*
+*@copyright Copyright 2017 Sudarshan Raghunathan
+*@file turtlebot.cpp
+*@author Sudarshan Raghunathan
+*@brief  Functions definitions for turtlebot class
+*/
+
+#include <geometry_msgs/Twist.h>
+#include <vector>
+#include "ros/ros.h"
+#include "ros/console.h"
+#include "turtlebot.hpp"
+#include "line_follower_turtlebot/pos.h"
+
+void turtlebot::dir_sub(line_follower_turtlebot::pos msg) {
+    turtlebot::dir = msg.direction;
+}
+
+void turtlebot::vel_cmd(geometry_msgs::Twist &velocity,
+ ros::Publisher &pub, ros::Rate &rate) {
+
+    // If direction is left
+    if (turtlebot::dir == 0) {
+        velocity.linear.x = 0.12;//0.1
+        velocity.angular.z = 0.45;//0.15
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("Turning Left");
+    }
+    // If direction is straight
+    if (turtlebot::dir == 1) {
+        velocity.linear.x = 0.14;//0.15
+        velocity.angular.z = 0;
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("Straight");
+    }
+    // If direction is right
+    if (turtlebot::dir == 2) {
+        velocity.linear.x = 0.10;
+        velocity.angular.z = -0.45;
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("Turning Right");
+    }
+    // If robot has to search
+    if (turtlebot::dir == 3) {
+        velocity.linear.x = 0;
+        velocity.angular.z = -0.9;
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("Searching");
+    }
+    // stop robot
+    if (turtlebot::dir == 4) {
+        velocity.linear.x = 0;
+        velocity.angular.z = 0;
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("Stop");
+    }
+    // robot search to stop left
+    if (turtlebot::dir == 5) {
+        velocity.linear.x = 0.04;//0.1
+        velocity.angular.z = 0.3;//0.15
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("park Left");
+    }
+    if (turtlebot::dir == 6) {
+    
+        velocity.linear.x = 0.04;
+        velocity.angular.z = -0.3;
+        pub.publish(velocity);
+        rate.sleep();
+        ROS_INFO_STREAM("park Right");
+    }
+
+    // robot search to stop left
+    if (turtlebot::dir == 7) {
+    velocity.linear.x = 0.04;//0.15
+    velocity.angular.z = 0;
+    pub.publish(velocity);
+    rate.sleep();
+    ROS_INFO_STREAM("park Straight");
+    }
+
+
+}
